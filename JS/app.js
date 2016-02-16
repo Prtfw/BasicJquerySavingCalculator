1543a4e4b13e90f3d1b24732e4056c643e07f3b9
prtfw <shang.madeleine@gmail.com>
2016-02-16 00:38:54 +0000

    scope problem

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,23 @@
 // function inside each event 
 // end obj proprties with comma then add next
 var formdata;
+var monthConv = {};
+
+	monthConv = {
+								'Jan': 0,
+								'Feb': 1,
+								'Mar': 2,
+								'Apr': 3,
+								'May': 4,
+								'Jun': 5,
+								'Jul': 6,
+								'Aug': 7,
+								'Sept': 8,
+								'Oct': 9,
+								'Nov': 10,
+								'Dec': 22,
+	};
+
 
 $(document).ready(
  	
@@ -8,6 +25,7 @@ $(document).ready(
 	function savinCalc(){
 		
 		function upddata(){
+			alert('update data called')
  			
  			formdata = {
 			'goal': $('#savingGoal').val(),
@@ -16,15 +34,24 @@ $(document).ready(
 			'amt':$('#savingAmount').val(),
 			'curAmt':$('#currentAmount').val(),
 			'monAmt':$('#monthlyAmount').val(),
+			
 		};
+						//alert(formdata.month)
+						//console.log(formdata)
+			return formdata
  	}
 		//alert("loaded");
 
 		$('#monthlyAmount').on("focusout", function() {
-			//alert('input change');
-			upddata();
-	
+			alert('input change');
+		 var formdata =	upddata();
+		 		console.log('backfrom updatedata');
+				console.log('main form data: ' + formdata);
+			//	alert(monthConv.formdata.month);
 				var res = calc(formdata);
+				
+
+				
 				//console.log('*'+res)
 				$('#res').text(res);
 				
@@ -66,22 +93,29 @@ $(document).ready(
 
 
 
-)
+);
 
 function calc(formdata){
-	//alert("calc called");
+	console.log("calc called");
+	//alert(monthConv.formdata.month);
+	
+	var goalDate= new Date(Number(formdata.yr), monthConv.formdata.month, 0);
 	var today = new Date();
 	var curMon = today.getMonth();
 	var curYr = today.getYear();
-
+	alert(goalDate + " vs " + today);
+	var month = goalDate-today
+	alert(month)
+	
+	
 	var month = 10; 
 	var totSav = 0;
 	var totSav = Number(month)*Number(formdata.monAmt) + Number(formdata.curAmt)
 
 	var res;
 
-	alert(month+ ' * ' +formdata.monAmt+' + ' + formdata.curAmt + ' = ' + totSav + ' vs. ' + formdata.amt);
-
+	//alert(month+ ' * ' +formdata.monAmt+' + ' + formdata.curAmt + ' = ' + totSav + ' vs. ' + formdata.amt);
+	
 
 	if (totSav < Number(formdata.amt)){
 		res=false
